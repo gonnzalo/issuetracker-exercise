02cb528c8c0f6e35403cac2ba9f7851af856447d
Gonzalo <arecogonzalo@gmail.com>
2019-03-29 08:37:30 +0800

    did PUT

--- routes/api.js
+++ routes/api.js
@@ -7,21 +7,81 @@
  */
 
 const { expect } = require("chai");
+const { ObjectID } = require("mongodb");
 
-module.exports = app => {
+module.exports = (app, db) => {
   app
     .route("/api/issues/:project")
 
     .get((req, res) => {
       const { project } = req.params;
+      const collection = db.collection(project);
+      collection
+        .find()
+        .toArray()
+        .then(items => {
+          return res.send(items);
+        })
+        .catch(err => console.error(`Failed to find documents: ${err}`));
     })
 
     .post((req, res) => {
       const { project } = req.params;
+      const newIssue = {
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_on: new Date(),
+        updated_on: new Date(),
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || "",
+        open: true,
+        status_text: req.body.status_text || ""
+      };
+
+      if (!newIssue.issue_title || !newIssue.issue_text || !newIssue.created_by)
+        return res.send("missing inputs");
+
+      const collection = db.collection(project);
+
+      return collection
+        .insertOne(newIssue)
+        .then(response => {
+          res.json(response.ops[0]);
+        })
+        .catch(err => console.error(`Failed to insert item: ${err}`));
     })
 
     .put((req, res) => {
       const { project } = req.params;
+
+      const collection = db.collection(project);
+
+      const updatedFields = {};
+
+      Object.keys(req.body).forEach(key => {
+        if (req.body[key] && key !== "_id") {
+          updatedFields[key] = req.body[key];
+        }
+      });
+
+      if (Object.keys(updatedFields).length === 0) {
+        res.send("no updated field sent");
+      }
+
+      collection
+        .findOneAndUpdate(
+          { _id: ObjectID(req.body._id) },
+          {
+            $set: updatedFields
+          },
+          { returnOriginal: false }
+        )
+        .then(updatedDocument => {
+          return res.send("successfully updated");
+        })
+        .catch(err =>
+          console.error(`Failed to find and update document: ${err}`)
+        );
     })
 
     .delete((req, res) => {
--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { expect } = require("chai");
 const cors = require("cors");
+const helmet = require("helmet");
+const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
 const apiRoutes = require("./routes/api.js");
 const fccTestingRoutes = require("./routes/fcctesting.js");
 const runner = require("./test-runner");
 
+const MONGODB_CONNECTION_STRING = process.env.DB;
+
 const app = express();
+
+app.use(helmet());
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
@@ -30,15 +36,24 @@ app.route("/").get((req, res) => {
 // For FCC testing purposes
 fccTestingRoutes(app);
 
-// Routing for API
-apiRoutes(app);
+// Connect to MongoDB
+const client = new MongoClient(MONGODB_CONNECTION_STRING, {
+  useNewUrlParser: true
+});
+client.connect(err => {
+  const db = client.db("tracker");
+  // perform actions on the collection object
 
-// 404 Not Found Middleware
-app.use((req, res, next) => {
-  res
-    .status(404)
-    .type("text")
-    .send("Not Found");
+  // Routing for API
+  apiRoutes(app, db);
+
+  // 404 Not Found Middleware
+  app.use((req, res, next) => {
+    res
+      .status(404)
+      .type("text")
+      .send("Not Found");
+  });
 });
 
 // Start our server and tests!
